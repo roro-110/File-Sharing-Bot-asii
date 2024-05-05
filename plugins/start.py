b1ab9ec7bfdb457cb25d9258a00da28216b7be99
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-05-05 12:39:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [          
                 [
-                    InlineKeyboardButton( "Main Channel", url = "t.me/InkaLinks" ),
+                    InlineKeyboardButton( "Main Channel", url = "https://t.me/InkaLinks" ),
                     InlineKeyboardButton("Source Code", url = "https://t.me/+nrNgQ7sT3XQxZTc1")
                 ], [ InlineKeyboardButton("Bot Update", url = "https://t.me/Publicfille") ],
                 [
@@ -128,7 +128,7 @@ async def not_joined(client: Client, message: Message):
         [
            InlineKeyboardButton(
                 "Join Channel",
-                url = "https://t.me/+m7XrPIdf6QUxZWM0"),
+                url = "https://t.me/+8OvF7hDbhD0zNzQ0"),
             InlineKeyboardButton(
                 "Join Channel",
                 url = client.invitelink)

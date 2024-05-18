a6298cddcb879e5bef589e139b5ea3d84204f323
taki0786 <119153138+taki0786@users.noreply.github.com>
2024-05-18 16:48:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text = f"<b>👨‍💻 𝘋𝘦𝘷𝘭𝘰𝘱𝘦𝘳 :</b> <a href='https://t.me/Zorosan110'>Roronoa Zoro</a> \n<b> 🤖 𝘊𝘳𝘦𝘢𝘵𝘰𝘳 :</b> <a href='t.me/zorosan110'> ᴄʜɪᴘs</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
-                [ [ InlineKeyboardButton(" Source code ", url="https://t.me/+NeqCUg-QDxo2Nzll"),
+                [ [ InlineKeyboardButton(" Source code ", url="https://telegra.ph/Source-Code-09-14-3"),
                   InlineKeyboardButton("Bot Channel" , url= "https://t.me/publicfille")],
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")

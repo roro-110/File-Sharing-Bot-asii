01d0d45bea953ec21097efaa81ba041d39757e56
roro-110 <khanafsarakhan@gmail.com>
2024-05-20 14:16:13 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>👨‍💻 𝘋𝘦𝘷𝘭𝘰𝘱𝘦𝘳 :</b> <a href='https://t.me/Zorosan110'>Roronoa Zoro</a> \n<b> 🤖 𝘊𝘳𝘦𝘢𝘵𝘰𝘳 :</b> <a href='t.me/zorosan110'> ᴄʜɪᴘs</a></b>",
+            text = f"<b>👨‍💻 𝘋𝘦𝘷𝘭𝘰𝘱𝘦𝘳 :</b> <a href='https://t.me/Zorosan110'>Roronoa Zoro</a> \n<b> 🤖 𝘊𝘳𝘦𝘢𝘵𝘰𝘳 :</b> <a href='t.me/zorosan110'> zoro</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
-                [ [ InlineKeyboardButton(" Source code ", url="https://telegra.ph/Source-Code-09-14-3"),
-                  InlineKeyboardButton("Bot Channel" , url= "https://t.me/apna_akatsuki")],
+                [ [ InlineKeyboardButton(" Source code ", url="https://t.me/FRIENDS_HINDI_ENGLISH_CHAT_GROUP"),
+                  InlineKeyboardButton("Bot Channel" , url= "https://t.me/FRIENDS_HINDI_ENGLISH_CHAT_GROUP")],
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
